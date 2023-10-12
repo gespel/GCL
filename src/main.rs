652e2bbdc4eba9d75d047c9d5e2078cfbf2723a4
sheimbrodt <sheimbrodt@psi.de>
2023-10-12 16:13:17 +0200

    added server

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ mod note;
 
 use std::{env, io};
 use std::io::Write;
+use std::net::{TcpListener, TcpStream};
 use std::process::exit;
 use crate::note::GCLNote;
 
@@ -20,8 +21,15 @@ impl CommandParser {
                 io::stdout().flush().expect("Fehler beim Schreiben auf stdout");
                 let mut input= String::new();
                 io::stdin().read_line(&mut input).expect("Error on stdin");
-                let mut input_split = input.split_whitespace().map(|s| s.to_string()).collect();
+                let input_split = input.split_whitespace().map(|s| s.to_string()).collect();
                 self.parse_commands(g, input_split);
+            }
+        }
+        else if basecmd == "serve" {
+            let listener = TcpListener::bind("127.0.0.1:1234").unwrap();
+            for stream in listener.incoming() {
+                let s = stream.unwrap();
+                g.handle_connection(s);
             }
         }
         else if basecmd == "exit" {
@@ -40,6 +48,14 @@ struct GCL;
 impl GCL {
     fn create_note(&self, name: String, note: String) {
         GCLNote::new(name, note).write_to_file();
+    }
+    fn handle_connection(&self, mut stream: TcpStream) {
+        let r = "\
+        HTTP/1.1 200 OK\r\n\
+        Content-Type: application/json\r\n\r\n\
+        {\"name\": \"Sten\"}
+        ";
+        stream.write_all(r.as_bytes());
     }
 }
 
